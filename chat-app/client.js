f8ead997b650f18f1f387c690acd18c5294666ed
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-05-25 00:35:07 -0700

    made cl more usable

--- chat-app/client.js
+++ chat-app/client.js
@@ -6,21 +6,63 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const clearLine = (dir) => {
+  return new Promise((res, rej) => {
+    process.stdout.clearLine(dir, () => {
+      res();
+    })
+  })
+}
+
+const moveCursor = (dx, dy) => {
+  return new Promise((res, rej) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      res();
+    })
+  })
+}
+
+let id;
+
 const client = net.createConnection({ port: 3008, host: "127.0.0.1" },
   async () => {
     console.log("connected to server")
 
-    const msg = await rl.question("Enter a message > ");
-    client.write(msg);
+    const ask = async () => {
+      const msg = await rl.question("Enter a message > ");
+      // clear the current line the cursor is in
+      await moveCursor(0, -1);
+      await clearLine(0)
+      client.write(`${id}-message-${msg}`);
+    }
+
+    ask();
+    client.on("data", async (data) => {
+      console.log();
+      await moveCursor(0, -1)
+      await clearLine();
+
+      if (data.toString("utf-8").substring(0, 2) === "id") {
+        id = data.toString("utf-8").substring(3);
+        console.log(`Your id is ${id}! \n`)
+
+      } else {
+        console.log(data.toString("utf-8"));
+      }
+      ask();
+    })
+
 
   })
 
+client.on("close", () => {
 
-client.on("data", (data) => {
-  console.log(data.toString());
 })
 
 
+
+
 client.on("end", () => {
+
   console.log("Connection was ended");
 })
--- chat-app/server.js
+++ chat-app/server.js
@@ -5,15 +5,33 @@ const clients = [];
 
 server.on("connection", (socket) => {
   console.log("A new connection to the server");
+  const clientId = clients.length + 1;
+
+  clients.map(c => {
+    c.socket.write(`User ${clientId} joined !`)
+  })
+
+  socket.write(`id-${clientId}`)
+
+
 
   socket.on("data", (data) => {
-    socket.write(data);
-    clients.forEach((c) => {
-      c.write(data);
+    clients.forEach((s) => {
+
+      s.socket.write(data);
     })
   })
-  clients.push(socket);
+
+  socket.on("end", () => {
+    clients.map(c => {
+      c.socket.write(`User ${clientId} left :(`)
+    })
+  })
+
+  clients.push({ id: clientId, socket })
+
 })
+
 
 
 
