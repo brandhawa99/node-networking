2f512f0f37140c0764a0df0cf99bbe8a4e89334f
Baltej Randhawa <baltej.randhawa0@gmail.com>
2023-05-24 20:26:52 -0700

    created basic client

---
+++ chat-app/client.js
@@ -0,0 +1,28 @@
+const net = require("net");
+const readline = require('readline/promises');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const client = net.createConnection({ port: 3008, host: "127.0.0.1" },
+  async () => {
+    console.log("connected to server")
+
+    const msg = await rl.question("Enter a message > ");
+    client.write(msg);
+
+  })
+
+
+client.on('close', () => {
+  console.log("close")
+})
+client.on("data", (data) => {
+  console.log(data.toString());
+})
+
+client.on("end", () => {
+  console.log("Connection was ended");
+})
